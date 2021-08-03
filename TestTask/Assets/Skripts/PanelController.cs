0caefc458ddf2554cad9e789c158f6deeaedb564
RoadFoster <roadfoster@outlook.com>
2021-08-03 21:10:59 +0300

    Add Project

---
+++ TestTask/Assets/Skripts/PanelController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelController : MonoBehaviour
+{
+    public FixedJoystick joy;
+    public float speed;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+        Quaternion rotX = Quaternion.AngleAxis(speed, new Vector3 (1, 0, 0));
+        Quaternion rotXM = Quaternion.AngleAxis(speed, new Vector3(-1, 0, 0));
+        Quaternion rotZ = Quaternion.AngleAxis(speed, new Vector3(0, 0, 1));
+        Quaternion rotZM = Quaternion.AngleAxis(speed, new Vector3(0, 0, -1));
+        
+        if (joy.Horizontal < 0)
+        {
+            transform.rotation *= rotZ;
+            
+        }
+        if (joy.Horizontal > 0)
+        {
+            transform.rotation *= rotZM;
+            
+        }
+        if (joy.Vertical > 0)
+        {
+            transform.rotation *= rotX;
+
+        }
+        if (joy.Vertical < 0)
+        {
+            transform.rotation *= rotXM;
+
+        }
+        
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.rotation*= rotX;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.rotation*= rotXM;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.rotation *= rotZ;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.rotation *= rotZM;
+        }
+    }
+}
---
+++ TestTask/Assets/Skripts/Trigger.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Trigger : MonoBehaviour
+{
+    public GameObject Boll;
+    public GameObject Panel;
+
+    public GameObject StartGame;
+    public GameObject Reset;
+    public GameObject Exit;
+
+    public Text Timer;
+    public GameObject GameOver;
+
+    public double milisec = 0;
+    public int sec = 0;
+    public int min = 0;
+    public double miliscreen;
+
+    private void Awake()
+    {
+        Time.timeScale = 0;
+        milisec = 0;
+    }
+    private void Start()
+    {
+        Reset.SetActive(false);
+        Exit.SetActive(false);
+        GameOver.SetActive(false);
+        
+    }
+    private void Update()
+    {
+        milisec = milisec + 1 * Time.deltaTime;
+        milisec += Time.deltaTime;
+        if (miliscreen >= 99)
+        {
+            miliscreen = 0;
+            sec++;
+            return;
+        }
+        if (sec >= 60)
+        {
+            min++;
+            sec = 0;
+            return;
+        }
+
+        if (milisec >= 0.01 && Time.timeScale == 1)
+        {
+                miliscreen++;
+                milisec = 0;
+                return;
+        }
+
+        Timer.text = min.ToString() + ":" + sec.ToString() + ":" + miliscreen.ToString();
+    }
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "boll")
+        {
+            Debug.Log("GameOver");
+            Reset.SetActive(true);
+            Exit.SetActive(true);
+            GameOver.SetActive(true);
+            Time.timeScale = 0;
+        }
+    }
+    public void Play()
+    {
+        StartGame.SetActive(false);
+        Time.timeScale = 1;
+        Reset.SetActive(false);
+        Exit.SetActive(false);
+        GameOver.SetActive(false);
+    }
+    public void reset()
+    {
+        Play();
+        milisec = 0;
+        sec = 0;
+        min = 0;
+        Boll.transform.position = new Vector3(0, 7, 12);
+        Boll.transform.rotation = Quaternion.Euler(0, 0, 0);
+        Panel.transform.rotation = new Quaternion(0, 0, 0, 0);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
